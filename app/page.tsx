375763b48bc587cf5a130a572f28c3fd907a5f37
abbasalquliyevszr2009-sudo <abbasalquliyevszr2009@gmail.com>
2026-06-20 19:11:51 +0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>Sayt yeniləndi!</h1>
+      <p>Artıq bura tamamilə təmiz və mənim nəzarətimdədir.</p>
+    </div>
+  );
+}
