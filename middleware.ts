55883443ca23e9039faa5aea9d9d9b955e35adfb
abbasalquliyevszr2009-sudo <abbasalquliyevszr2009@gmail.com>
2026-06-22 10:17:36 +0400

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname.startsWith('/admin')) {
+    const basicAuth = req.headers.get('authorization');
+
+    if (basicAuth) {
+      const authValue = basicAuth.split(' ')[1];
+      const [user, pwd] = atob(authValue).split(':');
+
+      if (user === 'admin' && pwd === 'WAlquliyevabbasGemiShop-2009_W') {
+        return NextResponse.next();
+      }
+    }
+
+    return new NextResponse('Giriş qadağandır!', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Secure Area"',
+      },
+    });
+  }
+  return NextResponse.next();
+}
+
