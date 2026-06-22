00b06bdc268ee65c1547bf139d9c4319c645a3fb
abbasalquliyevszr2009-sudo <abbasalquliyevszr2009@gmail.com>
2026-06-22 12:48:25 +0400

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,18 +1,5 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  const authHeader = request.headers.get('authorization');
-  
-  if (authHeader === 'Basic YWRtaW46V0FscXVsaXlldmFiYmFzR2VtaXNob3AtMjAwOV9X') {
-    return NextResponse.next();
-  }
-
-  return new NextResponse('Giriş qadağandır!', {
-    status: 401,
-    headers: { 'WWW-Authenticate': 'Basic' },
-  });
+  return NextResponse.next();
 }
-
-export const config = {
-  matcher: '/admin/:path*',
-};
