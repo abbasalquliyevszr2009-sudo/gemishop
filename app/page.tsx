1bdafb4a7df9569b2482d45ecc6b835da517708c
abbasalquliyevszr2009-sudo <abbasalquliyevszr2009@gmail.com>
2026-07-01 10:51:32 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 'use client' ;
+import Image from 'next/image';
+
 export default function Page() {
   return (
     <div>
       <h1>GemiShop</h1>
-      <img src="/file_000000003b9871f4985f9b18d4ca92a3.png" alt="GemiShop Logo" />
+      <Image 
+        src="/file_000000003b9871f4985f9b18d4ca92a3.png" 
+        alt="GemiShop Logo" 
+        width={300} 
+        height={300} 
+      />
       <p>Yenilandi</p>
     </div>
   );
