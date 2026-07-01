65bdc8a31f14bd19d13492b56f39ac53a62d2f2e
abbasalquliyevszr2009-sudo <abbasalquliyevszr2009@gmail.com>
2026-07-01 10:32:03 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ export default function Page() {
   return (
     <div>
       <h1>GemiShop</h1>
-      <p>Yeniləndi</p>
+      <img src="/file_000000003b9871f4985f9b18d4ca92a3.png" alt="GemiShop Logo" />
+      <p>Yenilandi</p>
     </div>
   );
 }
